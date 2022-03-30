8903656f5c796f6c4b9921c4e363eadb1ca4e341
Robert <robert.oelke90@web.de>
2022-03-30 23:36:12 +0200

    Projects and interfaces added.

---
+++ src/Kairos.Server/Agent.fs
@@ -0,0 +1,93 @@
+﻿namespace Kairos.Server
+
+type StatefullAgent<'msg, 'state> (initial : 'state, handleMsg : 'state -> 'msg -> Async<'state>) =
+  let errorEvent = Event<exn>()
+
+  let inbox = 
+    new MailboxProcessor<_>(fun inbox ->
+      let rec loop (current : 'state) =
+        async {
+          let! res = 
+            async {
+              let! msg = inbox.Receive()
+              return! handleMsg current msg
+            } |> Async.Catch
+
+          match res with
+          | Choice1Of2 newState ->
+            return! loop newState
+
+          | Choice2Of2 exn ->
+            errorEvent.Trigger exn
+            return! loop current
+        }
+
+      loop initial
+    )
+
+  member this.OnError = errorEvent.Publish
+
+  member this.Start () = inbox.Start ()
+
+  member this.Receive () = inbox.Receive ()
+
+  member this.Post (value : 'msg) = inbox.Post value
+
+  member this.PostAndReply (f : AsyncReplyChannel<'a> -> 'msg) = inbox.PostAndReply f
+
+  member this.PostAndAsyncReply (f : AsyncReplyChannel<'a> -> 'msg) = inbox.PostAndAsyncReply f
+
+  interface System.IDisposable with
+    member this.Dispose() =
+      (inbox :> System.IDisposable).Dispose()
+
+  static member Start(initialState, f) =
+    let agent = new StatefullAgent<_, _>(initialState, f)
+    agent.Start()
+    agent
+
+type StatelessAgent<'msg> (handleMsg : 'msg -> Async<unit>) =
+  let errorEvent = Event<exn>()
+
+  let inbox = 
+    new MailboxProcessor<_>(fun inbox ->
+      let rec loop () =
+        async {
+          let! res = 
+            async {
+              let! msg = inbox.Receive()
+              do! (handleMsg msg)
+            } |> Async.Catch
+
+          match res with
+          | Choice1Of2 () ->
+            ()
+          | Choice2Of2 exn ->
+            errorEvent.Trigger exn
+
+          return! loop ()
+        }
+
+      loop ()
+    )
+
+  member this.OnError = errorEvent.Publish
+
+  member this.Start () = inbox.Start ()
+
+  member this.Receive () = inbox.Receive ()
+
+  member this.Post (value : 'msg) = inbox.Post value
+
+  member this.PostAndReply (f : AsyncReplyChannel<'a> -> 'msg) = inbox.PostAndReply f
+
+  member this.PostAndAsyncReply (f : AsyncReplyChannel<'a> -> 'msg) = inbox.PostAndAsyncReply f
+
+  interface System.IDisposable with
+    member this.Dispose() =
+      (inbox :> System.IDisposable).Dispose()
+
+  static member Start f =
+    let agent = new StatelessAgent<_>(f)
+    agent.Start()
+    agent
---
+++ src/Kairos.Server/Types.fs
@@ -0,0 +1,41 @@
+﻿namespace Kairos.Server
+
+open System
+
+type EventSource = Guid
+type VersionNr = int64
+type EventName = string
+type EventJson = string
+
+type EventsToAppend<'event> = {
+  StreamSource : EventSource
+  Events : 'event list
+  ExpectedVersion : VersionNr option
+}
+
+type EventData<'event> = {
+  Source : EventSource
+  Version : VersionNr
+  RecordedAtUtc : DateTime
+  Event : 'event
+}
+
+type Aggregate<'data> = {
+  Source : EventSource
+  Version : VersionNr
+  RecordedAtUtc : DateTime
+  State : 'data
+}
+
+type EventEncoder<'event> = 'event -> EventName * EventJson
+type EventDecoder<'event> = EventName * EventJson -> 'event
+
+type IEventStore<'event> =
+  abstract member GetStream : EventSource -> Async<EventData<'event> list>
+  abstract member Append : EventsToAppend<'event> -> Async<unit>
+  abstract member OnError : IEvent<exn>
+  abstract member OnEvents : IEvent<EventData<'event> list>
+
+type IAggregateStore<'state, 'event> =
+  inherit IEventStore<'event>
+  abstract member GetAggregate : EventSource -> Async<Aggregate<'state> option>
