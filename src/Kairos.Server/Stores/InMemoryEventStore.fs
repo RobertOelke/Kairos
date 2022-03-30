cdffd2baf9ea6ea33c8ee0a6b020af187dbb193d
Robert <robert.oelke90@web.de>
2022-03-31 00:33:56 +0200

    InMemoryEventStore added

---
+++ src/Kairos.Server/Stores/InMemoryEventStore.fs
@@ -0,0 +1,66 @@
+﻿namespace Kairos.Server
+
+open System
+
+type private InMemoryEventStoreMsg<'event> =
+| GetStream of EventSource * AsyncReplyChannel<EventData<'event> list>
+| Append of EventsToAppend<'event> * AsyncReplyChannel<unit>
+
+type InMemoryEventStore<'event>() =
+  let errorEvent = Event<exn>()
+  let eventsAppended = Event<EventData<'event> list>()
+
+  let update (storage : EventData<'event> list) (msg : InMemoryEventStoreMsg<'event>) =
+    match msg with
+    | GetStream (src, reply) ->
+      async {
+        let events = storage |> List.filter (fun e -> e.Source = src)
+
+        do reply.Reply(events)
+
+        return storage
+      }
+    | Append (events, reply) ->
+      async {
+        let oldEvents = storage |> List.filter (fun e -> e.Source = events.StreamSource)
+
+        let currentVersion =
+          match oldEvents with
+          | [] -> 0L
+          | lst -> lst |> List.map (fun x -> x.Version) |> List.max
+        
+        if events.ExpectedVersion |> Option.map ((<>) currentVersion) = Some true then
+          errorEvent.Trigger (new Exception("Append failed"))
+          reply.Reply()
+          return storage
+
+        else
+          let now = DateTime.UtcNow
+          let newEvents =
+            oldEvents
+            @ (events.Events
+              |> List.mapi (fun i e -> {
+                  Source = events.StreamSource
+                  Version = currentVersion + (int64 i) + 1L
+                  RecordedAtUtc = now
+                  Event = e
+                })
+              )
+              
+          eventsAppended.Trigger(newEvents)
+          reply.Reply()
+          return storage @ newEvents
+        }
+
+  let agent = StatefullAgent<'event, EventData<'event> list>.Start([], update)
+   
+  interface IEventStore<'event> with
+    member this.GetStream (src : EventSource) : Async<EventData<'event> list> =
+      agent.PostAndAsyncReply(fun reply -> GetStream (src, reply))
+
+    member this.Append (events : EventsToAppend<'event>) : Async<unit> =
+      agent.PostAndAsyncReply(fun reply -> Append (events, reply))
+
+    member this.OnError : IEvent<exn> = errorEvent.Publish
+
+    member this.OnEvents : IEvent<EventData<'event> list> = eventsAppended.Publish
---
+++ tests/Kairos.Server.Tests/Program.fs
@@ -0,0 +1,29 @@
+﻿module Kairos.Server.Tests
+
+open System
+open Kairos.Server
+open Expecto
+
+type TestEvent = 
+| EventOne of int
+| EventTwo of bool
+
+let tests =
+  testList "All Tests" [
+    testAsync "Append and get" {
+      let src = Guid.NewGuid()
+      let store = InMemoryEventStore<TestEvent>() :> IEventStore<TestEvent>
+
+      let events = [ EventOne 1; EventOne 2 ]
+
+      do! store.Append { StreamSource = src; ExpectedVersion = None; Events = events }
+
+      let! res = store.GetStream src
+      
+      Expect.equal res.Length 2 "Two events loaded"
+      Expect.equal (res |> List.map (fun e -> e.Event)) events "Appended events loaded"
+    }
+  ]
+
+runTestsWithCLIArgs [] [||] tests
+|> ignore
